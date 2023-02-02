76b610f0640cd631b91b0ef83c75a79c1581e43f
root <jedidiabahena@gmail.com>
2023-02-02 13:23:59 +0000

    added get_db function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -26,6 +26,21 @@ def get_logger() -> logging.Logger:
 
     return log
 
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Returns a connector to the database
+    """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    db_cnx = mysql.connector.connect(user=username,
+                                     password=password,
+                                     host=host,
+                                     database=db_name)
+    return db_cnx
+
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
