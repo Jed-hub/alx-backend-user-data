74e4f4ecad8eafef8efffb21c62af9f9b8c694b9
root <jedidiabahena@gmail.com>
2023-02-02 13:35:41 +0000

    added main function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -52,6 +52,27 @@ def filter_datum(fields: List[str], redaction: str,
     return message
 
 
+def main():
+    """
+    Obtains database connection using get_db
+    and retrieves all rows in the users table
+    and displays each row under a filtered format
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM USERS;")
+    fields_names = [i[0] for i in cursor.description]
+
+    logger = get_logger()
+
+    for row in cursor:
+        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
+        logger.info(str_row.strip())
+
+    cursor.close()
+    db.close()
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
