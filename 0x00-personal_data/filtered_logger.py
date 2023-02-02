b5e8f450a476ccabe5b72efd6da8840db6b4ca32
root <jedidiabahena@gmail.com>
2023-02-02 12:56:26 +0000

    fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -27,15 +27,12 @@ def get_logger() -> logging.Logger:
     return log
 
 
-def filter_datum(fields: List, redaction: str, message: str,
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """
-    Returns the log message obfuscated
-    """
+    """Returns the log message obfuscated"""
     for field in fields:
-        message = re.sub(f'{field}=.+?{separator}',
+        message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
-
     return message
 
 
