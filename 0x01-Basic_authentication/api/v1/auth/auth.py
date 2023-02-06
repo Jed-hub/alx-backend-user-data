cf2ddd992e96128a2774868a9b7e37534189c060
root <jedidiabahena@gmail.com>
2023-02-06 18:35:04 +0000

    updated reauire_auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,17 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require auth method
         """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path[-1] is not '/':
+            path += '/'
+
+        for paths in excluded_paths:
+            if path == paths:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header method
