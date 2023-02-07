f39738a1d2e0fbd6d3c5926085bbd8f6289b8bdc
root <jedidiabahena@gmail.com>
2023-02-07 14:19:12 +0000

    added function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -40,3 +40,19 @@ class BasicAuth(Auth):
             return None
 
         return decoded.decode('utf-8')
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """Returns the user email and password from
+        Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None\
+           or type(decoded_base64_authorization_header) != str\
+           or ':' not in decoded_base64_authorization_header:
+
+            return (None, None)
+
+        credentials = decoded_base64_authorization_header.split(':', 1)
+
+        return (credentials[0], credentials[1])
