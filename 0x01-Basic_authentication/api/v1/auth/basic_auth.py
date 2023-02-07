4adf83f14f5c74cf8ac15bd0bb458d9d92b9c510
root <jedidiabahena@gmail.com>
2023-02-07 13:51:53 +0000

    added function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,18 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic authentication class
     """
+    def extract_base64_authorization_header(
+                                            self,
+                                            authorization_header: str
+                                            ) -> str:
+        """Returns the Base64 part of the authorization
+        header for Basic Authentication
+        """
+        if authorization_header is None\
+           or type(authorization_header) != str\
+           or not authorization_header.startswith('Basic ')\
+           and not authorization_header.endswith(' '):
+
+            return None
+
+        return authorization_header.split(' ')[1]
