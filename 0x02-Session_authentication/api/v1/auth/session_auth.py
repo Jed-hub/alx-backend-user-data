4b349c280a936cb1e5d17ee36c994a9c0fde2919
root <jedidiabahena@gmail.com>
2023-02-08 19:15:42 +0000

    added create session attribute

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,22 @@
 """Session auth module
 """
 from api.v1.auth.auth import Auth
+from typing import Dict
+from uuid import uuid4, UUID
 
 
 class SessionAuth(Auth):
     """Sessions Authentication class
     """
+    user_id_by_session_id: Dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a session ID for a user_id
+        """
+        if user_id is None or type(user_id) != str:
+            return None
+
+        session_id: str = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
