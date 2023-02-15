334132a22e7de4d069f58a6503228bf8793ca1b9
root <jedidiabahena@gmail.com>
2023-02-15 17:02:02 +0000

    added session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ app module"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -30,6 +30,27 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
     return jsonify({"email": email, "message": "user created"})
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """ Creates a session for the user and stores it the sessionID
+    as a cookie with key "session_id" on the response and returns a JSON
+    payload of the form
+    """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(401)
+
+    if (AUTH.valid_login(email, password)):
+        session_id = AUTH.create_session(email)
+        if session_id is not None:
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie("session_id", session_id)
+            return response
+
+    abort(401)
 
 
 if __name__ == "__main__":
